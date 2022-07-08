75d02603e2bd53933784ae9077953e7aa84019ce
Sai Manideep Kuna <sai.manideep.kuna@ibm.com>
2022-07-09 01:33:38 +0530

    generated to string method for node

--- src/linkedlist/Node.java
+++ src/linkedlist/Node.java
@@ -1,6 +1,14 @@
 package linkedlist;
 
 public class Node {
+    @Override
+    public String toString() {
+        return "Node{" +
+                "data=" + data +
+                ", next=" + next +
+                '}';
+    }
+
     int data;
     Node next;
 }
